1ef474ed561fef0dc09ccaf17bd217385dcbefea
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2022-04-27 18:39:08 +0500

    Bazadaga ma'lumotlarni chiqarish

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ mongoose.connect(
     console.log("Connected to database");
   }
 );
+
 app.use(express.static("public"));
 app.use(expressEdge.engine);
 app.set("views", `${__dirname}/views`);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.render("index", { posts });
 });
 
 app.get("/about", (req, res) => {
